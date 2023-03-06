e282f531f4f86565983f6daa472bf7d9302a7e2a
nkrief4 <93315184+nkrief4@users.noreply.github.com>
2023-03-06 12:23:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ def print_matrix(alphabet, states, initial, final, transitions):
     list_states = []
 
     for i in states:
-
         # FOR ARROW / FIRST COLUMN
         list_initial.append(get_arrow(i, initial, final))
 
@@ -90,10 +89,47 @@ def print_matrix(alphabet, states, initial, final, transitions):
     print(x)
 
 
+def est_standard(alphabet, states, initial, final, transitions):
+    """
+    Vérifie si un automate est standard ou non.
+    Un automate est standard si:
+    - il a un seul état initial
+    - tous les états finaux sont distincts
+    - il n'y a pas de transitions avec epsilon comme symbole d'entrée
+    - chaque transition a un seul symbole d'entrée
+    """
+    # Vérifier s'il y a un seul état initial
+    if len(initial) != 1:
+        return False
+
+    # Vérifier que tous les états finaux sont distincts
+    if len(set(final)) != len(final):
+        return False
+
+    # Vérifier s'il y a des transitions avec epsilon comme symbole d'entrée
+    for transition in transitions:
+        if transition[2] == 'epsilon':
+            return False
+
+    # Vérifier que chaque transition a un seul symbole d'entrée
+    for state in states:
+        symbols = set()
+        for transition in transitions:
+            if transition[0] == state:
+                symbol = transition[2]
+                if symbol in symbols:
+                    return False
+                symbols.add(symbol)
+
+    # Si toutes les conditions sont satisfaites, l'automate est standard
+    print("Automate standard")
+    return True
+
+
 if __name__ == '__main__':
     print('BEGIN\n\n')
 
     alphabet, states, initial, final, list_transitions = ouverture(
         "automata_test.txt")
-
+    est_standard(alphabet, states, initial, final, list_transitions)
     print_matrix(alphabet, states, initial, final, list_transitions)
