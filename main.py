c0f60f031998671ceb6c7cc0c3e16fa4d5f2d4be
Cyril Nakhla <cyrilnakhla@MacBook-Pro-de-Cyril.local>
2023-03-06 22:27:25 +0100

    mise à jour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from table_display import print_matrix
 
 # Fonction pour filtrer les données d'une ligne
+
+
 def filtrer_list(list_name, num_line, first_char, separator):
     return list_name[num_line][first_char:].split(separator)
 
@@ -10,7 +13,7 @@ def ouverture(url):
     with open(url, 'r') as f:
         lines = f.read().splitlines()
 
-        alphabet = filtrer_list(lines, 0, 4, ',')
+        alphabet = filtrer_list(lines, 0, 4, ', ')
 
         states = filtrer_list(lines, 1, 4, ',')
 
@@ -22,25 +25,40 @@ def ouverture(url):
         for line in lines[4:]:
             list_transitions.append(line)
 
-    return alphabet, states, initial_state, final_state
+    return alphabet, states, initial_state, final_state, list_transitions
 
 
 if __name__ == '__main__':
     print('BEGIN\n\n')
 
-    alphabet, states, initial, final = ouverture("automata_test.txt")
+    alphabet, states, initial, final, list_transitions = ouverture(
+        "automata_test.txt")
+
+    print(alphabet, states, initial, final, list_transitions)
+    print_matrix(alphabet, states, initial, final, list_transitions)
 
 
-def is_deterministic(states, start_states, accept_states):
+def is_deterministic(alphabet, states, initial, final, transitions):
  # Vérification des états initiaux
-    if len(start_states) != 1:
+    if len(initial) != 1:
         return False
-    if start_states[0] not in states:
+    if initial[0] not in states:
         return False
 
     # Vérification des états d'acceptation
     for accept_state in accept_states:
         if accept_state not in states:
             return False
+
+    # Vérifier que chaque transition a un seul symbole d'entrée
+    for state in states:
+        letter = set()
+        for transition in transitions:
+            if transition[0] == state:
+                actual_letter = transition[2]
+                if actual_letter in letter:
+                    print("3")
+                    return False
+                letter.add(actual_letter)
 
     return True
