af5b09f41efad8f2851c44b9394375c4b5373a20
Cyx <65498535+Cyrilnkl@users.noreply.github.com>
2023-03-26 01:15:39 +0100

    Update INT3_5_complementary.py

--- INT3_5_complementary.py
+++ INT3_5_complementary.py
@@ -1,11 +1,14 @@
 from INT3_5_table_display import print_matrix
 
 
-def complement(alphabet, states, initial, final, transitions):
+def complement(alphabet, states, initial, final, transitions, n):
 
     new_array = []                                              # We just put as final all states that weren't
     for s in states:
         if s not in final:
             new_array.append(s)
     final = new_array
+
+    if n == 1 :
+        return final
     print_matrix(alphabet, states, initial, final, transitions)
